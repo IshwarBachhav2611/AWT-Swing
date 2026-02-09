def9173c4ca1f119450b81ff4813eb1f17276932
IshwarBachhav2611 <ishwarbachhav2611@gmail.com>
2026-02-09 22:05:19 +0530

    [ADDED]ishwar: Basic of Frame in AWT Java

---
+++ BasicFrame.java
@@ -0,0 +1,9 @@
+import java.awt.*;
+
+public class BasicFrame {
+    public static void main(String[] args){
+        Frame f = new Frame("Basic Frame");
+        f.setSize(400,500);
+        f.setVisible(true);
+    }
+}
